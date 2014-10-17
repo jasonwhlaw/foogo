de519b0f20078a47e5cf27e32c36a02c91e9bc55
Jason Law <jasonlaw89@gmail.com>
2014-10-17 17:12:15 +0800

    beautify the page layout by modifying the grid of the html

--- app/controllers/food_item_controller.rb
+++ app/controllers/food_item_controller.rb
@@ -1,7 +1,11 @@
 class FoodItemController < ApplicationController
 
   def show
-    render 'show'
+
+    @id_requested = params[:id]
+    @show_item = FoodItem.where(id: @id_requested).map { |item| item.name }
+    # @show_item = FoodItem.where(id: @id_requested).pluck('name')
+    # @show_item = FoodItem.where(id: @id_requested).pluck('name')
   end
 
 
