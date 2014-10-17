6f16b66307d6576e2503f66c2a2b28f738b827c6
Jason Law <jasonlaw89@gmail.com>
2014-10-17 15:32:01 +0800

    added daily special from different cusine categories

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,18 +3,24 @@ class WelcomeController < ApplicationController
   def index
 
 
+    # @item = FoodItem.all.sample
+    @itemAsian = FoodItem.where(cuisine: "Asian").sample
+    @itemWestern = FoodItem.where(cuisine: "Western").sample
+    @itemAdventurous = FoodItem.where(cuisine: "Adventurous").sample
+
     if params[:admin] == "true"
 
       current_time = Time.now
       # 45 minutes in seconds
       @mealtime = current_time + (45*60)
+
       render 'index'
-
-
 
     else
       render 'under_construction'
     end
+
+
 
   end
 end
